02a87aee1ad379a507d671d0804c429ca5d8bb94
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2017-04-05 20:14:57 -0300

    aprendendo aquelas bases em js

---
+++ javascript/client/js/app/models/Negociacao.js
@@ -0,0 +1,32 @@
+class Negociacao {
+
+	constructor(date, quantity, value) {
+		this._data = new Date(date.getTime());
+		this._quantidade = quantity; 
+		this._valor = value;
+		//garante que os atributos nao serao alterados
+		//posteriormente
+		
+		Object.freeze(this);
+
+	}
+
+	get volume() {
+		return this._quantidade * this._valor; 
+	} 
+
+	get data() {
+		return new Date(this._data.getTime());
+
+	}
+
+	get quantidade () {
+		return this._quantidade; 
+	}
+
+	get valor() {
+		return this._valor;
+	}
+
+
+}

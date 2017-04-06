a5632ece2245157ca685348f991fbeb35c13a87c
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2017-04-06 12:22:32 -0300

    expressão regular, helpers e model

--- javascript/client/js/app/controllers/NegociacaoController.js
+++ javascript/client/js/app/controllers/NegociacaoController.js
@@ -6,27 +6,36 @@ class NegociacaoController {
 		this._inputData =$("#data");
 		this._inputQuantidade = $("#quantidade");
 		this._inputValor = $("#valor");
+		this._listaNegociacoes = new ListaNegociacoes();
 	}
+
 
 	adiciona(event) {
 
 		event.preventDefault();
-
-		let helper = new DateHelper();
-
-		let data = helper.textToDate(this._inputData.value);
-
-		console.log(data);
-
-		let negociacao = new Negociacao(
-			data, 
-			this._inputQuantidade.value, 
-			this._inputValor.value);
-
-		let dataOut = helper.dateToText(negociacao.data);
-
 		
-	
+		this._listaNegociacoes.adiciona(this._createNegociacao());
+		this._cleanInputFields();
+
+	}
+
+
+	_createNegociacao() {
+
+		return  new Negociacao( 
+			DateHelper.textToDate(this._inputData.value),
+			 this._inputQuantidade.value, 
+			this._inputValor.value);
+	}
+
+	_cleanInputFields() {
+
+		this._inputData.value = "";
+		this._inputQuantidade.value = 1; 
+		this._inputValor.value = 0.0; 
+
+		this._inputData.focus();
+
 	}
 
 
--- javascript/client/js/app/helpers/DateHelper.js
+++ javascript/client/js/app/helpers/DateHelper.js
@@ -1,20 +1,28 @@
 class DateHelper {
 
 
-	textToDate(text){
+	constructor(){
+		
+		throw new Error("DateHelper cannot be instantiated.");	
+	}
+
+	static textToDate(text){
+		
+		if(!/\d{4}-\d{2}-\d{2}/.test(text))
+			throw new Error('Date must be invalid. Need be yyyy-mm-aa');
 
 		return new Date(...text.split("-").map((item, index) => item - index%2));
-
+  
 	}
 
-	dateToText(date){
+	
+	static dateToText(date){
 
-		return date.getDate() 
-			+ "/" + (date.getMonth() + 1),
-			+ "/" + date.getFullYear();
+		return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+
 
 	}
-
+ 
 
 
 }

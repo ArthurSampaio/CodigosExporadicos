fd7c4110742777e1e66db705c31620b0df37bc5b
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2017-05-30 10:41:08 -0300

    engatinhando no anglar2

---
+++ angular2/hello-world/src/app/first-my/my-first.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core'
+
+@Component({
+	selector: 'my-first-component',
+	template : ` 
+		<p>My First Component with Angular2!</p>
+
+	`
+})
+
+export class MyFirstComponent {
+
+
+
+
+}

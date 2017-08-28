e9b5649a2296c38a2fffa2d12354216652abdcc2
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2017-08-28 17:08:23 -0300

    function whick get the exactly sorted time

--- R/sorting_comparison.R
+++ R/sorting_comparison.R
@@ -5,6 +5,9 @@ library(FrF2)
 first.algorithm <- "QUICK"
 second.algorithm <- "MERGE"
 
+RANDOM <- "RANDOM"
+REVERSE <- "REVERSE"
+
 heap <- read.csv2("~/SortingAlgorithmsMC/heap.txt")
 merge <- read.csv2("~/SortingAlgorithmsMC/merge.txt")
 quick <- read.csv2("~/SortingAlgorithmsMC/quick.txt")
@@ -12,6 +15,10 @@ quick <- read.csv2("~/SortingAlgorithmsMC/quick.txt")
 heap$time <- as.double(heap$time)
 merge$time <- as.double(merge$time)
 quick$time <- as.double(quick$time)
+
+heap <- heap %>% filter(array_type != "SORTED") %>% mutate(size_type = size/90000)
+merge <- merge %>% filter(array_type != "SORTED") %>% mutate(size_type = size/90000)
+quick <- quick %>% filter(array_type != "SORTED") %>% mutate(size_type = size/90000)
 
 
 
@@ -24,40 +31,53 @@ ExperimentFactorial <- function(heap, merge, quick) {
                                                      size = c("Low", "High"),
                                                      type = c("REVERSE", "RANDOM")))
   
-  results = ResponseBalance(plan, merge, quick)
+  response.times <- do.call(rbind.data.frame, apply(plan, 1, FindRow, sort1 = quick, sort2 = merge))
+
+  plan.actual <- add.response(design = plan, response = response.times[4])
   
 }
 
-ResponseBalance <- function(plan, sort1, sort2) {
+
+FindRow <- function(line, sort1, sort2 ) {
   
   if(line[1] == first.algorithm){
-    if(line[2]/4500 && line[3] == "REVERSE"){
+    if(line[2] == "High"){
+      if(line[3] == RANDOM){
+        row <- sort1 %>% filter(size_type >= 1 & array_type == RANDOM) %>% sample_n(1)
+      }else{ #REVERSE
+        row <- sort1 %>% filter(size_type >= 1 & array_type == REVERSE) %>% sample_n(1)
+      }
       
+    }else{#LOW
+      if(line[3] == RANDOM){
+        row <- sort1 %>% filter(size_type < 1 & array_type == RANDOM) %>% sample_n(1)
+      }else{ #REVERSE
+        row <- sort1 %>% filter(size_type < 1 & array_type == REVERSE) %>% sample_n(1)
+      }
     }
+
   }else{
-    
+    if(line[2] == "High"){
+      if(line[3] == RANDOM){
+        row <- sort2 %>% filter(size_type >= 1 & array_type == RANDOM) %>% sample_n(1)
+      }else{ #REVERSE
+        row <- sort2 %>% filter(size_type >= 1 & array_type == REVERSE) %>% sample_n(1)
+      }
+      
+    }else{#LOW
+      if(line[3] == RANDOM){
+        row <- sort2 %>% filter(size_type < 1 & array_type == RANDOM) %>% sample_n(1)
+      }else{ #REVERSE
+        row <- sort2 %>% filter(size_type < 1 & array_type == REVERSE) %>% sample_n(1)
+      }
+    }
   }
-  
-  
+
+  return (row)
 }
 
 
-FindRow <- function(line, sort1, sort2) {
-  
 
-}
-
-FindSizeType <- function (tamanho) {
-  print(tamanho)
-
-  if(tamanho > 45000){
-    type <- "High"
-  }else{
-    type <- "Low"
-  }
-  
-  return (type)
-}
 
 
 
